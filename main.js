920ad5bd4907cef8f737ea4dadc169776993bcd9
ANZA BAJWA <161083667+Anzabajwa@users.noreply.github.com>
2024-05-24 13:31:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// //var with hoisting
+// console.log(number1)//declaration hoisted to the top but initiailization is not
+// var number1 : string = "anza sarwar"
+// console.log(number1)
+// // let with hoisting
+// console.log(number)
+//  let number : number = 17
+//  console.log(number)
+console.log(number2);
+var number = 10;
---
+++ main.ts
@@ -0,0 +1,19 @@
+//var with hoisting
+
+console.log(myName)//declaration hoisted to the top but initiailization is not
+var myName : string = "anza sarwar"
+console.log(myName)
+
+
+// let with hoisting
+
+
+console.log(number)
+
+ let number : number = 17
+
+
+ console.log(number)
+
+
+
